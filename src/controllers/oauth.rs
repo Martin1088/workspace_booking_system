4d30c8d1d576955083a5f0a7a11d1475a8394d4b
Martin Jurk <m049k@dkfz-vpn2390.inet.dkfz-heidelberg.de>
2025-08-28 08:45:12 +0200

    test slean up

--- src/controllers/oauth.rs
+++ src/controllers/oauth.rs
@@ -88,7 +88,7 @@ pub async fn authentik_authorization_url(
             Error::InternalServerError
         })?;
     
-    let auth_url = get_authorization_url_with_pkce(session, &mut client).await;
+    let auth_url = get_authorization_url(session, &mut client).await;
     info!("Auth URL: {}", auth_url);
     drop(client);
     Ok(auth_url)
@@ -156,6 +156,8 @@ pub async fn authentik_callback_cookie(
     drop(client);
     Ok(response)
 }
+
+
 
 async fn protected(
     State(ctx): State<AppContext>,
